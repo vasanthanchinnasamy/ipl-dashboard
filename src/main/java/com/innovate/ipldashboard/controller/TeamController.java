d4feae2d3a2de90120318fa050be2881daaae46f
Vasanthan C <vasanthantamilan@gmail.com>
2021-05-21 23:43:13 +0530

    Added React

--- src/main/java/com/innovate/ipldashboard/controller/TeamController.java
+++ src/main/java/com/innovate/ipldashboard/controller/TeamController.java
@@ -7,11 +7,13 @@ import com.innovate.ipldashboard.model.Team;
 import com.innovate.ipldashboard.repository.MatchRepository;
 import com.innovate.ipldashboard.repository.TeamRepository;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private  TeamRepository teamRepository;
